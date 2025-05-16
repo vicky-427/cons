dd56cb4a3bb18bd83110a9342f63984bb9d0e459
vicky-427 <vigneshvk0427@gmail.com>
2025-05-16 19:18:11 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // You can create this file for global styles
+import './index.css'; // Global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
